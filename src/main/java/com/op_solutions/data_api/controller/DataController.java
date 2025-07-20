89cbd31cbdba38580b303a8cea4ebae2aae81dc1
Omkar Upadhyay <omkarmh009@gmail.com>
2025-07-20 21:09:04 +0530

    Added ExceptionHandling in DataRestController when no data to display

--- src/main/java/com/op_solutions/data_api/controller/DataController.java
+++ src/main/java/com/op_solutions/data_api/controller/DataController.java
@@ -6,14 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.op_solutions.data_api.dtos.ErrorResponse;
 import com.op_solutions.data_api.entity.User;
+import com.op_solutions.data_api.exceptions.ResourceNotFoundException;
 import com.op_solutions.data_api.service.DataService;
 
 @RestController
 @RequestMapping("/api/data")
+
 public class DataController {
 	
 	@Autowired
@@ -21,15 +25,34 @@ public class DataController {
 	
 	
 	@GetMapping("/users")
-	public ResponseEntity<List<User>> getAllUsers()
+	public ResponseEntity<Object> getAllUsers()
 	{
 		
 		if(service.getAllUsersData() != null && service.getAllUsersData().size() > 0)
 		{
 			return new ResponseEntity(service.getAllUsersData(), HttpStatus.OK);
 		}
+		else {
+			throw new ResourceNotFoundException("All Users Data not found");
+		}
 		
-		return new ResponseEntity(HttpStatus.NO_CONTENT);
+		
+		
+		
+		
+	}
+	
+	
+	@GetMapping("/user/{userId}")
+	public ResponseEntity<User> getUserFromUserId(@PathVariable("userID") Long userId)
+	{
+		
+		if(service.getUserByUserId(userId) != null)
+		{
+			return new ResponseEntity<User>(service.getUserByUserId(userId), HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
 		
 		
 	}
