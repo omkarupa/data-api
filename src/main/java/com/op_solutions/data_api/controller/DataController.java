1dff8c5b6e69a756fb2f6a980458824be145ad4b
Omkar Upadhyay <omkarmh009@gmail.com>
2025-07-20 20:29:02 +0530

    fetching data from service for all users list

--- src/main/java/com/op_solutions/data_api/controller/DataController.java
+++ src/main/java/com/op_solutions/data_api/controller/DataController.java
@@ -1,23 +1,37 @@
 package com.op_solutions.data_api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.op_solutions.data_api.entity.User;
+import com.op_solutions.data_api.service.DataService;
 
 @RestController
 @RequestMapping("/api/data")
 public class DataController {
 	
+	@Autowired
+	DataService service;
+	
 	
 	@GetMapping("/users")
-	public ResponseEntity<User> getAllUsers()
+	public ResponseEntity<List<User>> getAllUsers()
 	{
-		return new ResponseEntity(null, HttpStatus.OK);
+		
+		if(service.getAllUsersData() != null && service.getAllUsersData().size() > 0)
+		{
+			return new ResponseEntity(service.getAllUsersData(), HttpStatus.OK);
+		}
+		
+		return new ResponseEntity(HttpStatus.NO_CONTENT);
+		
+		
 	}
 	
 	
